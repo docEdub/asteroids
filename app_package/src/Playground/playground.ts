0df7890af52f2827ef69b29eaecf126fd9ee5826
Andy Fillebrown <afillebrown@microsoft.com>
2022-08-12 14:06:18 -0400

    Use "Simple Sinusoid Bouncing Ball" playground
    See https://playground.babylonjs.com/#8WLKPR

--- app_package/src/Playground/playground.ts
+++ app_package/src/Playground/playground.ts
@@ -3,32 +3,18 @@ import "@babylonjs/loaders";
 
 class Playground {
     public static CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
-        // This creates a basic Babylon Scene object (non-mesh)
-        var scene = new BABYLON.Scene(engine);
-
-        // This creates and positions a free camera (non-mesh)
-        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-        // This targets the camera to scene origin
-        camera.setTarget(BABYLON.Vector3.Zero());
-
-        // This attaches the camera to the canvas
-        camera.attachControl(canvas, true);
-
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
-
-        // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-        // Move the sphere upward 1/2 its height
+        const scene = new BABYLON.Scene(engine);
+        const sphere = BABYLON.Mesh.CreateSphere("sphere", 16, 2, scene);
         sphere.position.y = 1;
+        BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+        scene.createDefaultCameraOrLight();
 
-        // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-        var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+        scene.onBeforeRenderObservable.runCoroutineAsync(function* () {
+            for (let frameCount = 0; true; ++frameCount) {
+                sphere.position.y = 1 + 2 * Math.abs(Math.sin(frameCount / 16));
+                yield;
+            }
+        }());
 
         return scene;
     }
