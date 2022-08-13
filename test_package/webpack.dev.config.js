366e413e892ef735a44c61c3eaf4f4d91722a279
Andy Fillebrown <andy.fillebrown@gmail.com>
2022-08-12 23:22:59 -0400

    Silence source-map URL warnings in browser

--- test_package/webpack.dev.config.js
+++ test_package/webpack.dev.config.js
@@ -9,6 +9,7 @@ module.exports = {
         open: true,
         watchFiles: ["./src/**/*", "../app_package/lib/**/*"]
     },
+    devtool: "eval-cheap-source-map",
     output: {
         path: path.resolve(__dirname, "../docs"),
         filename: "bundle.js"
